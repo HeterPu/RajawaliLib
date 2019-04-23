206b201b46614356ba77318808b25436468920ad
HeterPu <wycgpeterhu@sina.com>
2019-04-24 00:38:29 +0800

    ddd

--- rajawali/src/main/java/org/rajawali3d/cameras/ArcballCamera.java
+++ rajawali/src/main/java/org/rajawali3d/cameras/ArcballCamera.java
@@ -103,8 +103,8 @@ public class ArcballCamera extends Camera {
 
     private void mapToScreen(final float x, final float y, Vector2 out)
     {
-        out.setX(x / mLastWidth);
-        out.setY(y / mLastHeight);
+        out.setX(mScreenMapping * x / mLastWidth);
+        out.setY(-mScreenMapping * y / mLastHeight);
     }
 
     private void startRotation(final float x, final float y)
